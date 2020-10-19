9f8b0e9ac6f7d978bafe11af354e81056f285e0c
safinsingh <safin.singh@gmail.com>
2020-10-19 14:12:33 -0700

    feat: add support for printf-style messages

--- log.go
+++ log.go
@@ -10,25 +10,106 @@ import (
 // Success prints a successful message to the console
 // and prepends it with [SCSS] in bold green text.
 func Success(message string) {
-	fmt.Println(aurora.Green(aurora.Bold("[SCSS]")), message)
+	fmt.Println(
+		aurora.Green(
+			aurora.Bold("[SCSS]"),
+		),
+		message,
+	)
 }
 
 // Info prints an informational message to the console
 // and prepends it with [INFO] in bold cyan text.
 func Info(message string) {
-	fmt.Println(aurora.Cyan(aurora.Bold("[INFO]")), message)
+	fmt.Println(
+		aurora.Cyan(
+			aurora.Bold("[INFO]"),
+		),
+		message,
+	)
 }
 
 // Warn prints a warning message to the console
 // and prepends it with [WARN] in bold yellow text.
 func Warn(message string) {
-	fmt.Println(aurora.Yellow(aurora.Bold("[WARN]")), message)
+	fmt.Println(
+		aurora.Yellow(
+			aurora.Bold("[WARN]"),
+		),
+		message,
+	)
 }
 
 // Fail prints an error message to the console
 // and prepends it with [FAIL] in bold red text.
 // It also exits the program with an exit code of 1.
 func Fail(message string) {
-	fmt.Println(aurora.Red(aurora.Bold("[FAIL]")), message)
+	fmt.Println(
+		aurora.Red(
+			aurora.Bold("[FAIL]"),
+		),
+		message,
+	)
+	os.Exit(1)
+}
+
+// SuccessF prints a successful message given
+// a format string to the console and prepends
+// it with [SCSS] in bold green text.
+func SuccessF(format string, message ...interface{}) {
+	fmt.Println(
+		aurora.Green(
+			aurora.Bold("[SCSS]"),
+		),
+		fmt.Sprintf(
+			format,
+			message...,
+		),
+	)
+}
+
+// InfoF prints an informational message given
+// a format string to the console and prepends
+// it with [SCSS] in bold yellow text.
+func InfoF(format string, message ...interface{}) {
+	fmt.Println(
+		aurora.Cyan(
+			aurora.Bold("[INFO]"),
+		),
+		fmt.Sprintf(
+			format,
+			message...,
+		),
+	)
+}
+
+// WarnF prints a warning message given
+// a format string to the console and prepends
+// it with [WARN] in bold yellow text.
+func WarnF(format string, message ...interface{}) {
+	fmt.Println(
+		aurora.Yellow(
+			aurora.Bold("[WARN]"),
+		),
+		fmt.Sprintf(
+			format,
+			message...,
+		),
+	)
+}
+
+// FailF prints an error message given
+// a format string to the console and prepends
+// it with [FAIL] in bold red text.
+func FailF(format string, message ...interface{}) {
+	fmt.Println(
+		aurora.Red(
+			aurora.Bold("[FAIL]"),
+		),
+		fmt.Sprintf(
+			format,
+			message...,
+		),
+	)
 	os.Exit(1)
 }
