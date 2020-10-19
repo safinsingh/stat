955c9edad21af185ca376227daa61ddc54fc6c0d
safinsingh <safin.singh@gmail.com>
2020-10-19 11:53:11 -0700

    docs: add

--- log.go
+++ log.go
@@ -7,19 +7,28 @@ import (
 	"github.com/logrusorgru/aurora"
 )
 
+// Success prints a successful message to the console
+// and prepends it with [SCSS] in bold green text.
 func Success(message string) {
 	fmt.Println(aurora.Green(aurora.Bold("[SCSS]")), message)
 }
 
+// Info prints an informational message to the console
+// and prepends it with [INFO] in bold cyan text.
 func Info(message string) {
 	fmt.Println(aurora.Cyan(aurora.Bold("[INFO]")), message)
 }
 
+// Warn prints a warning message to the console
+// and prepends it with [WARN] in bold yellow text.
 func Warn(message string) {
 	fmt.Println(aurora.Yellow(aurora.Bold("[WARN]")), message)
 }
 
-func Fatal(message string) {
+// Fail prints an error message to the console
+// and prepends it with [FAIL] in bold red text.
+// It also exits the program with an exit code of 1.
+func Fail(message string) {
 	fmt.Println(aurora.Red(aurora.Bold("[FATL]")), message)
 	os.Exit(1)
 }
